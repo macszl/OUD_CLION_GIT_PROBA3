9239a2c4e658810d933fd0a429766ad38fa588e1
macszl <235989@edu.p.lodz.pl>
2021-02-01 17:08:30 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/OUD_CLION_GIT_PROBA3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OUD_CLION_GIT_PROBA3.dir/main.cpp.obj"
+  "OUD_CLION_GIT_PROBA3.exe"
+  "OUD_CLION_GIT_PROBA3.exe.manifest"
+  "OUD_CLION_GIT_PROBA3.pdb"
+  "libOUD_CLION_GIT_PROBA3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OUD_CLION_GIT_PROBA3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/OUD_CLION_GIT_PROBA3.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for OUD_CLION_GIT_PROBA3.
+# This may be replaced when dependencies are built.
